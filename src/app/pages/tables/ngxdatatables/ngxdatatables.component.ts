1cab65a57eb89705f5a3dd470f88e54b42b92796
Donovan Kozak <donovan.kozak@nephys.com>
2023-12-29 10:17:25 +0100

    test

--- src/app/pages/tables/ngxdatatables/ngxdatatables.component.ts
+++ src/app/pages/tables/ngxdatatables/ngxdatatables.component.ts
@@ -1,5 +1,5 @@
 import { Component, NgModule, OnInit, ViewChild } from "@angular/core";
-import { ModalDirective, ModalModule } from 'ngx-bootstrap/modal';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { PetitPokemon } from './petit_pokemon';
 
 //@NgModule({ imports: [ModalModule.forRoot()] })
@@ -17,186 +17,42 @@ export enum SelectionType {
 })
 export class NgxDatatablesComponent implements OnInit {
 
-  @ViewChild('addPokemonModal') addPokemonModal: ModalDirective;
+  closeResult: string;
 
-  entries: number = 5;
-  selected: any[] = [];
-  temp: PetitPokemon[] = [];
-  activeRow: any;
-  rows: PetitPokemon[] = [
-    {
-      id: 1,
-      name: "Bulbizarre",
-      hp: 25,
-      cp: 5,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
-      types: ["Plante", "Poison"]
-  },
-  {
-      id: 2,
-      name: "Salamèche",
-      hp: 28,
-      cp: 6,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
-      types: ["Feu"]
-  },
-  {
-      id: 3,
-      name: "Carapuce",
-      hp: 21,
-      cp: 4,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
-      types: ["Eau"]
-  },
-  {
-      id: 4,
-      name: "Aspicot",
-      hp: 16,
-      cp: 2,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/013.png",
-      types: ["Insecte", "Poison"]
-  },
-  {
-      id: 5,
-      name: "Roucool",
-      hp: 30,
-      cp: 7,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png",
-      types: ["Normal", "Vol"]
-  },
-  {
-      id: 6,
-      name: "Rattata",
-      hp: 18,
-      cp: 6,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/019.png",
-      types: ["Normal"]
-  },
-  {
-      id: 7,
-      name: "Piafabec",
-      hp: 14,
-      cp: 5,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/021.png",
-      types: ["Normal", "Vol"]
-  },
-  {
-      id: 8,
-      name: "Abo",
-      hp: 16,
-      cp: 4,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/023.png",
-      types: ["Poison"]
-  },
-  {
-      id: 9,
-      name: "Pikachu",
-      hp: 21,
-      cp: 7,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png",
-      types: ["Electrique"]
-  },
-  {
-      id: 10,
-      name: "Sabelette",
-      hp: 19,
-      cp: 3,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/027.png",
-      types: ["Normal"]
-  },
-  {
-      id: 11,
-      name: "Mélofée",
-      hp: 25,
-      cp: 5,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/035.png",
-      types: ["Fée"]
-  },
-  {
-      id: 12,
-      name: "Goupix",
-      hp: 17,
-      cp: 8,
-      picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/037.png",
-      types: ["Feu"]
-  },
-  {
-    id: 13,
-    name: "Tarsal",
-    hp: 13,
-    cp: 6,
-    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/280.png",
-    types: ["Psy", "Fée"]
-  }
-  ];
-  SelectionType = SelectionType;
+  constructor(private modalService: NgbModal) {}
 
-  newPokemon: any = {};
-
-  constructor() {
-    this.temp = this.rows.map((pokemon) => {
-      return { ...pokemon };
-    });
-  }
-
-  getTypeColor(type: string): any {
-    const defaultStyle = {
-      'background-color': 'gray',
-      'color': 'white' // Couleur de texte
-    };
-
-    switch (type.toLowerCase()) {
-      case 'plante':
-        return { 'background-color': 'green', 'color': 'white' };
-      case 'insecte':
-        return { 'background-color': 'brown', 'color': 'white' };
-      case 'electrique':
-        return { 'background-color': 'yellow', 'color': 'gray', 'font-size': '10.5px' };
-      case 'feu':
-        return { 'background-color': 'red', 'color': 'white' };
-      case 'eau':
-        return { 'background-color': 'blue', 'color': 'white' };
-      case 'vol':
-        return { 'background-color': 'skyblue', 'color': 'white' };
-      case 'combat':
-        return { 'background-color': 'orange', 'color': 'white' };
-      case 'psy':
-        return { 'background-color': 'darkviolet', 'color': 'white' };
-      case 'poison':
-        return { 'background-color': 'mediumorchid', 'color': 'white' };
-      case 'fée':
-        return { 'background-color': 'hotpink', 'color': 'white' };
-      default:
-        return defaultStyle;
+  open(content, type, modalDimension) {
+      if (modalDimension === 'sm' && type === 'modal_mini') {
+          this.modalService.open(content, { windowClass: 'modal-mini', size: 'sm', centered: true }).result.then((result) => {
+              this.closeResult = 'Closed with: $result';
+          }, (reason) => {
+              this.closeResult = 'Dismissed $this.getDismissReason(reason)';
+          });
+      } else if (modalDimension === '' && type === 'Notification') {
+        this.modalService.open(content, { windowClass: 'modal-danger', centered: true }).result.then((result) => {
+            this.closeResult = 'Closed with: $result';
+        }, (reason) => {
+            this.closeResult = 'Dismissed $this.getDismissReason(reason)';
+        });
+      } else {
+          this.modalService.open(content,{ centered: true }).result.then((result) => {
+              this.closeResult = 'Closed with: $result';
+          }, (reason) => {
+              this.closeResult = 'Dismissed $this.getDismissReason(reason)';
+          });
+      }
     }
-  }
 
-  open() {
-    this.newPokemon = {}; // Réinitialiser les données du nouveau Pokémon
-    this.addPokemonModal.show();
-  }
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return  'with: $reason';
+        }
+    }
+    ngOnInit() {
+    }
 
-  addPokemon() {
-    this.rows.push({ ...this.newPokemon, types: [] }); // Ajouter le nouveau Pokémon à la liste
-    this.closeModal();
   }
-
-  closeModal() {
-    this.addPokemonModal.hide();
-  }
-
-  entriesChange($event) {
-    this.entries = $event.target.value;
-  }
-
-  onSelect({ selected }) {
-    this.selected.splice(0, this.selected.length);
-    this.selected.push(...selected);
-  }
-
-  onActivate(event) {
-    this.activeRow = event.row;
-  }
-
-  ngOnInit() {}
-}
